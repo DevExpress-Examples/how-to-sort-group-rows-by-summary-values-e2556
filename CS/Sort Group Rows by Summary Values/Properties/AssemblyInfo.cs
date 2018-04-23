7c2e624cefb686d7966a107b0a4a3aa628825de1
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Sort Group Rows by Summary Values/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Sort_Group_Rows_by_Summary_Values")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sort_Group_Rows_by_Summary_Values")]
+[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("45a348d3-d670-400c-8207-38e192392173")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VB/Sort Group Rows by Summary Values/MainPage.xaml.vb
@@ -0,0 +1,39 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Windows.Controls
+Imports DevExpress.Xpf.Grid
+
+Namespace Sort_Group_Rows_by_Summary_Values
+    Partial Public Class MainPage
+        Inherits UserControl
+
+        Public Sub New()
+            InitializeComponent()
+            grid.ItemsSource = (New AccountList()).GetData()
+            SortGroupsBySummary(view.GroupedColumns(0))
+        End Sub
+        Private Sub SortGroupsBySummary(ByVal column As GridColumn)
+            Dim sortInfo As New GridGroupSummarySortInfo(grid.GroupSummary(0), column.FieldName, System.ComponentModel.ListSortDirection.Ascending)
+            grid.GroupSummarySortInfo.Add(sortInfo)
+        End Sub
+    End Class
+    Public Class AccountList
+        Public Function GetData() As List(Of Account)
+            Return CreateAccounts()
+        End Function
+        Private Function CreateAccounts() As List(Of Account)
+            Dim list As New List(Of Account)()
+            list.Add(New Account() With {.UserName = "Nick White", .RegistrationDate = Date.Today, .Age = 57})
+            list.Add(New Account() With {.UserName = "Jack Rodman", .RegistrationDate = New Date(2009, 5, 10), .Age = 24})
+            list.Add(New Account() With {.UserName = "Sandra Sherry", .RegistrationDate = New Date(2009, 5, 10), .Age = 35})
+            list.Add(New Account() With {.UserName = "Sabrina Vilk", .RegistrationDate = Date.Today, .Age = 19})
+            list.Add(New Account() With {.UserName = "Mike Pearson", .RegistrationDate = New Date(2008, 10, 18), .Age = 42})
+            Return list
+        End Function
+    End Class
+    Public Class Account
+        Public Property UserName() As String
+        Public Property RegistrationDate() As Date
+        Public Property Age() As Integer
+    End Class
+End Namespace
